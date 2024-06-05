a411828ae876f6ed0d70c40d87ac30a186e453d6
Izabela Leme <izabela.leme@plasutil.com.br>
2024-06-05 08:33:27 -0300

    first commit

---
+++ Modelos/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop.Modelos;
+
+internal class Pet
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Especie { get; set; }
+
+
+}
